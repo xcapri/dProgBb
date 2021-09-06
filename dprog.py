18517bf6f57b08adc358e9bbe9bc344374777f2f
XCapri <43540712+xcapri@users.noreply.github.com>
2021-09-06 23:09:10 +0700

    Update dprog.py

--- dprog.py
+++ dprog.py
@@ -35,7 +35,18 @@ class x504f4e424f545f424f554e54:
 			"vulnerability report",
 			"report-a-vulnerability",
 			"legal-security",
-			"bug-bounty"
+			"bug-bounty",
+			"security-bug-report",
+			"white_hat",
+			"terms-bounty",
+			"security_policy",
+			"support-vulnerability",
+			"securitydisclosureprogram",
+			"report-vulnerability",
+			"contribute-bug-bounty",
+			"report-security-issues",
+			"responsible_disclosure",
+			"bug-beacon"
 		]
     def x62616e6e6572(self): 
 		print("""{}
@@ -74,22 +85,7 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
 			open(self.Outputs+"/"+title, "a").write(url+"\n")
 		except:
 			pass
-    
-    def x63656b436f6465(self, domain): # cek status code
-        self.x676173476f6c6574(domain)
-		# try:
-		# 	r = requests.get(domain, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"}, timeout=20, allow_redirects=False)
-			
-		# 	if r.status_code == 200:
-		# 		self.x676173476f6c6574(domain)
-		# 	elif r.status_code == 302:
-		# 		self.x676173476f6c6574(domain)
-		# 	elif r.status_code == 301:
-		# 		self.x676173476f6c6574(domain)
-		# 	else:
-		# 		print ("{}[!] Can't Access {}".format(self.red, domain))
-        # except:
-		# 	print ("{}[!] Can't Access {}".format(self.red, domain))
+
     def x676173476f6c6574(self, domain): # buat ngerequest
         try:
             url = domain
@@ -103,7 +99,7 @@ x4c6f6164436c617373 = x504f4e424f545f424f554e54() # manggil class
 x4c6f6164436c617373.x62616e6e6572()               # manggil banner
 x6c69737472616e646f6d776562 = [i.strip() for i in open(str(raw_input("List : "))).readlines()]  # ngingput dan load list
 x62616e74657265 = Pool(int(input('Thread : ')))   # input jumlah threads
-x62616e74657265.map(x4c6f6164436c617373.x63656b436f6465, x6c69737472616e646f6d776562)           # lets go 
+x62616e74657265.map(x4c6f6164436c617373.x676173476f6c6574, x6c69737472616e646f6d776562)           # lets go 
 
 # refrensi code :
 # aHR0cHM6Ly9naXRodWIuY29tL2p1c3Rha2F6aC9MYUNyb3Q= (Akazh)
