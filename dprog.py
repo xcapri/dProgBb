cdba810a80bc2ce444f040b45f3f3468445a79e0
XCapri <43540712+xcapri@users.noreply.github.com>
2021-09-08 13:17:58 +0700

    Update dprog.py

--- dprog.py
+++ dprog.py
@@ -48,9 +48,9 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
 			""".format(random.choice(self.kolor_lo), "Detect Program Bug Bounty",  "Mung nggo golet-golet", self.date))
     
     
-    def x636865636b(self, content, url): #cek apakah ada keyword yg berbau program bug bounty
+    def x636865636b(self, body, url): #cek apakah ada keyword yg berbau program bug bounty
         try:
-            body =  content.text
+            body =  body.text
             for key in self.keybounty:
                 if key in body:
                     print (self.green+"[!] Found  "+key+" on{} {}".format(self.white, url))
@@ -58,7 +58,7 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
                 else:
                     print (self.red+"[!] Not found  "+key+" on "+url)
         except:
-			pass
+		pass
     
     def x73617665(self, title, url): # function buat save result
 		try:
@@ -68,18 +68,29 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
 
     def x676173476f6c6574(self, domain): # buat ngerequest
         try:
-            url = domain
-            r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"})
-            self.x636865636b(r, url)
+            if domain.startswith(('http://', 'https://')):
+                url = domain
+                r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"})
+                self.x636865636b(r, url)
+            else:
+                url = 'http://'+domain
+                r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"}, allow_redirects=True)
+                self.x636865636b(r, r.url)
         except:
-			pass
+		pass
 
-
+# try:
 x4c6f6164436c617373 = x504f4e424f545f424f554e54() # manggil class
 x4c6f6164436c617373.x62616e6e6572()               # manggil banner
 x6c69737472616e646f6d776562 = [i.strip() for i in open(str(raw_input("List : "))).readlines()]  # ngingput dan load list
 x62616e74657265 = Pool(int(input('Thread : ')))   # input jumlah threads
-x62616e74657265.map(x4c6f6164436c617373.x676173476f6c6574, x6c69737472616e646f6d776562)           # lets go 
+
+try:
+    x62616e74657265.map(x4c6f6164436c617373.x676173476f6c6574, x6c69737472616e646f6d776562)     # lets go 
+except KeyboardInterrupt:
+        print("CTRL+C Detect, Exit!")
+        exit()
+
 
 # refrensi code :
 # aHR0cHM6Ly9naXRodWIuY29tL2p1c3Rha2F6aC9MYUNyb3Q= (Akazh)
