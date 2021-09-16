d1b7dd3ceedbbd59fa069c01e9123ae2167168de
XCapri <43540712+xcapri@users.noreply.github.com>
2021-09-16 12:44:20 +0700

    Update dprog.py

--- dprog.py
+++ dprog.py
@@ -28,6 +28,9 @@ class x504f4e424f545f424f554e54:
 		
 		# tambah keyword di file listkey.txt
 		self.keybounty = [i.strip() for i in open('listkey.txt').readlines()]
+		self.pathbounty = [i.strip() for i in open('path.txt').readlines()]
+
+
     def x62616e6e6572(self): 
 		print("""{}
 
@@ -50,13 +53,14 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
     
     def x636865636b(self, body, url): #cek apakah ada keyword yg berbau program bug bounty
         try:
-            body =  body.text
+            bodyx =  body.text
             for key in self.keybounty:
-                if key in body:
+                if key in bodyx:
                     print (self.green+"[!] Found  "+key+" on{} {}".format(self.white, url))
                     self.x73617665("key-"+key+".txt", url)
                 else:
                     print (self.red+"[!] Not found  "+key+" on "+url)
+
         except:
 		pass
     
@@ -68,14 +72,17 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
 
     def x676173476f6c6574(self, domain): # buat ngerequest
         try:
-            if domain.startswith(('http://', 'https://')):
-                url = domain
-                r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"})
-                self.x636865636b(r, url)
-            else:
-                url = 'http://'+domain
-                r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"}, allow_redirects=True)
-                self.x636865636b(r, r.url)
+            for pPath in self.pathbounty:
+
+                if domain.startswith(('http://', 'https://')):
+                    url = domain+pPath
+                    r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"})
+                    self.x636865636b(r, url)
+                else:
+                    url = 'http://'+domain+pPath
+                    r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"}, allow_redirects=True)
+                    hr = requests.get(r.url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; dProgBb) Gecko/20190101 Firefox/77.0"})
+                    self.x636865636b(hr, r.url)
         except:
 		pass
 
@@ -86,7 +93,7 @@ x6c69737472616e646f6d776562 = [i.strip() for i in open(str(raw_input("List : "))
 x62616e74657265 = Pool(int(input('Thread : ')))   # input jumlah threads
 
 try:
-    x62616e74657265.map(x4c6f6164436c617373.x676173476f6c6574, x6c69737472616e646f6d776562)     # lets go 
+    x62616e74657265.map(x4c6f6164436c617373.x676173476f6c6574, x6c69737472616e646f6d776562)           # lets go 
 except KeyboardInterrupt:
         print("CTRL+C Detect, Exit!")
         exit()
