4e443a646f506ed2d16383bf8a0590cf596949f3
XCapri <43540712+xcapri@users.noreply.github.com>
2021-11-01 09:20:37 +0700

    Update rDom.py

--- rDom.py
+++ rDom.py
@@ -2,11 +2,11 @@ import tldextract
 
 def exDom(dom):
     ext = tldextract.extract(dom)
-    open("testwl.txt", "a").write(ext.registered_domain+"\n")
+    open("out-root.txt", "a").write(ext.registered_domain+"\n")
     return ext.registered_domain
 
 
-listdom = [i.strip() for i in open('wl.txt').readlines()]
+listdom = [i.strip() for i in open('list-subdo.txt').readlines()]
 
 for dom in listdom:
     print(exDom(dom))
