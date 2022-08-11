a77968eef08e8bf86c5132b25ea7d96b81697f68
XCapri <43540712+xcapri@users.noreply.github.com>
2022-08-11 10:15:35 +0700

    Update dprog.py

--- dprog.py
+++ dprog.py
@@ -43,8 +43,8 @@ _/   _//_//        _//    _//  _//  _//  _//_/      _/_//   _//
                 full_url = "{}{}".format(domain,xpath)
                 getbody = requests.get(full_url, headers=self.headers,verify=False, allow_redirects=True, timeout=3) # request
                 if (getbody.status_code == 404 or getbody.status_code == 403 
-                    or "No route" in getbody.text or "Not Found" in getbody.text):
-                    print(Fore.RED+"[NOTFOUND] "+Fore.RESET+full_url+Fore.RED+" Not Found "+Fore.RESET)
+                    or "No route" in getbody.text or "Not Found" in getbody.text or "404" in getbody.text):
+                    print(Fore.RED+"[SKIPCHECK] "+Fore.RESET+full_url+Fore.RED+" Not Found "+Fore.RESET)
                 else:
                     self.checkKeyonResponse(getbody.text, full_url) # send body to key response
 	except ConnectionError:
