84c45a535e2689d4a8d84a5e4335abb686d8352a
pikpikcu <pikag78@gmail.com>
2023-09-06 15:47:42 +0700

    Update

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dProgBb',
+    version='1.0',
+    description='Detect Program Bug Bounty',
+    author='pikpikcu&Hasim',
+    author_email='N/A',
+    url='https://github.com/pikpikcu/dProgBb',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dprog=core.main:main',
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
